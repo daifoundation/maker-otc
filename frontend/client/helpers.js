fcf8a0f18624f314a7db1fb6722626de107dd405
Joris Bontje <jorisbontje@gmail.com>
2016-03-22 17:11:26 +0100

    configure base currency

--- frontend/client/helpers.js
+++ frontend/client/helpers.js
@@ -1,3 +1,7 @@
+Template.registerHelper('baseCurrency', function (value) {
+  return BASE_CURRENCY;
+})
+
 Template.registerHelper('formatPrice', function (value) {
   return Math.round(value * 100) / 100;
 })
