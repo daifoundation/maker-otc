ab5301cfd18702a52ae6d070b69d16861f2e108a
Joris Bontje <jorisbontje@gmail.com>
2016-03-23 11:12:58 +0100

    highlight cancelled orders

--- frontend/client/orderrow.js
+++ frontend/client/orderrow.js
@@ -1,7 +1,9 @@
 Template.orderRow.events({
   'click .cancel': function (event, template) {
     event.preventDefault()
-    var id = parseInt(template.data.order._id)
+    var _id = template.data.order._id;
+    var id = parseInt(_id)
+    Offers.update({_id: _id}, {$set: {cancelled: true}})
     var cancelTx = MakerOTC.cancel(id, {gas: 100000})
     // TODO locally mark as deleted
     console.log("cancel!", id, cancelTx)
