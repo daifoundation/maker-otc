d91605a814bc734f94a3a0233e09d1be3827334a
rainbeam <rainbeam@riseup.net>
2016-04-20 13:49:04 +0100

    change maker / taker -> seller / buyer

--- contracts/atomic_trade.sol
+++ contracts/atomic_trade.sol
@@ -52,18 +52,19 @@ contract AtomicTrade is MakerUser, EventfulMarket, FallbackFailer, Assertive {
         ItemUpdate(id);
         return id;
     }
-    function trade( address maker, address taker, uint make_how_much, bytes32 make_which_token, uint take_how_much, bytes32 take_which_token ) internal
+    function trade( address seller, uint sell_how_much, bytes32 sell_which_token, address buyer, uint buy_how_much, bytes32 buy_which_token ) internal
     {
-        transferFrom( taker, maker, take_how_much, take_which_token );
-        transfer( taker, make_how_much, make_which_token );
-        Trade( make_how_much, make_which_token, take_how_much, take_which_token );
+        transferFrom( buyer, seller, buy_how_much, buy_which_token );
+        transfer( buyer, sell_how_much, sell_which_token );
+        Trade( sell_how_much, sell_which_token, buy_how_much, buy_which_token );
     }
     function buy( uint id )
     {
         var offer = offers[id];
         assert(offer.active);
 
-        trade( offer.owner, msg.sender, offer.sell_how_much, offer.sell_which_token, offer.buy_how_much, offer.buy_which_token );
+        trade( offer.owner, offer.sell_how_much, offer.sell_which_token,
+               msg.sender, offer.buy_how_much, offer.buy_which_token );
 
         delete offers[id];
         ItemUpdate(id);
@@ -74,13 +75,15 @@ contract AtomicTrade is MakerUser, EventfulMarket, FallbackFailer, Assertive {
         assert(offer.active);
 
         if ( offers[id].sell_how_much <= quantity ) {
-            trade( offer.owner, msg.sender, offer.sell_how_much, offer.sell_which_token, offer.buy_how_much, offer.buy_which_token );
+            trade( offer.owner, offer.sell_how_much, offer.sell_which_token,
+                   msg.sender, offer.buy_how_much, offer.buy_which_token );
             delete offers[id];
 
         } else {
             uint buy_quantity = quantity * offers[id].buy_how_much / offers[id].sell_how_much;
             if ( buy_quantity > 0 ) {
-                trade( offer.owner, msg.sender, quantity, offer.sell_which_token, buy_quantity, offer.buy_which_token );
+                trade( offer.owner, quantity, offer.sell_which_token,
+                       msg.sender, buy_quantity, offer.buy_which_token );
 
                 offer.sell_how_much -= quantity;
                 offer.buy_how_much -= buy_quantity;
