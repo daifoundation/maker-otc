887992defe869ddbfd9beb16e604bc8e2943dc29
NTPape <NTPape@users.noreply.github.com>
2016-04-20 14:07:47 +0200

    makereth form viewmodel

--- frontend/client/templates/makereth.js
+++ frontend/client/templates/makereth.js
@@ -1,52 +1,31 @@
-Session.setDefault('depositType', 'deposit')
-Session.setDefault('depositAmount', '0')
-
-var amountMax = function () {
-  var depositType = Session.get('depositType')
-  if (depositType === 'deposit') {
-    return web3.fromWei(Session.get('ETHBalance'))
-  } else {
-    return web3.fromWei(Tokens.findOne('ETH').balance)
-  }
-}
-
-Template.makereth.helpers({
-  'depositType': function () {
-    return Session.get('depositType')
-  },
-  'amountMax': amountMax,
-  'buttonState': function () {
-    var depositType = Session.get('depositType')
-    var amount = new BigNumber(Session.get('depositAmount'))
-    if (depositType && amount.gt(0) && amount.lte(new BigNumber(amountMax()))) {
-      return ''
+Template.makereth.viewmodel({
+  type: 'deposit',
+  amount: '',
+  maxAmount: function () {
+    if (this.type() === 'deposit') {
+      return web3.fromWei(Session.get('ETHBalance'))
     } else {
-      return 'disabled'
+      return web3.fromWei(Tokens.findOne('ETH').balance)
     }
-  }
-})
-
-Template.makereth.events({
-  'change .radios': function () {
-    var depositType = $('input:radio[name="depositType"]:checked').val()
-    Session.set('depositType', depositType)
   },
-  'change input[name="depositAmount"], keyup input[name="depositAmount"], mouseup input[name="depositAmount"]': function () {
-    var depositAmount = $('input[name="depositAmount"]').val()
-    Session.set('depositAmount', depositAmount)
+  canDeposit: function () {
+    try {
+      var amount = new BigNumber(this.amount())
+      var maxAmount = new BigNumber(this.maxAmount())
+      return amount.gt(0) && amount.lte(maxAmount)
+    } catch (e) {
+      return false
+    }
   },
-  'click #deposit': function (event) {
+  deposit: function (event) {
     event.preventDefault()
 
-    var depositType = Session.get('depositType')
-    var depositAmount = Session.get('depositAmount')
     var options = { from: Session.get('address'), gas: 3141592 }
-
-    if (depositType === 'deposit') {
-      options['value'] = web3.toWei(depositAmount)
+    if (this.type() === 'deposit') {
+      options['value'] = web3.toWei(this.amount())
       Dapple['makerjs'].getToken('ETH').deposit(options)
     } else {
-      Dapple['makerjs'].getToken('ETH').withdraw(web3.toWei(depositAmount), options)
+      Dapple['makerjs'].getToken('ETH').withdraw(web3.toWei(this.amount()), options)
     }
   }
 })
