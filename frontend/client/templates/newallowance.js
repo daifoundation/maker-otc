fd52171b3cf6475dbb334c4f6abd80e7cfbb89c2
NTPape <NTPape@users.noreply.github.com>
2016-04-21 14:24:47 +0200

    Make new allowance forms reactive to allowance changes

--- frontend/client/templates/newallowance.js
+++ frontend/client/templates/newallowance.js
@@ -1,8 +1,25 @@
 Template.newallowance.viewmodel({
   value: '',
+  allowance: '',
+  autorun: function () {
+    // Initialize value and allowance
+    var _id = this.templateInstance.data.token._id
+    this.value(web3.fromWei(this.templateInstance.data.token.allowance))
+    this.allowance(this.templateInstance.data.token.allowance)
+
+    // Update allowance manually on change, because the form is not automatically reactive to Collection changes
+    var _this = this
+    Tokens.find().observeChanges({
+      changed: function (id, fields) {
+        if (id === _id && fields.hasOwnProperty('allowance')) {
+          _this.allowance(fields.allowance)
+        }
+      }
+    })
+  },
   canChange: function () {
     try {
-      return this.value() !== '' && web3.toWei(this.value()) !== this.templateInstance.data.token.allowance
+      return this.value() !== '' && this.value() !== web3.fromWei(this.allowance())
     } catch (e) {
       return false
     }
