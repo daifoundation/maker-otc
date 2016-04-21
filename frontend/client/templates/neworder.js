93ca3d07582c8625a1f8972eabb9d442f2d5536f
NTPape <NTPape@users.noreply.github.com>
2016-04-21 12:56:59 +0200

    neworder form viewmodel

--- frontend/client/templates/neworder.js
+++ frontend/client/templates/neworder.js
@@ -1,102 +1,87 @@
-Session.setDefault('ordertype', 'buy')
-Session.setDefault('currency', 'ETH')
-Session.setDefault('price', 0)
-Session.setDefault('amount', 0)
-Session.setDefault('total', 0)
-
-var totalMax = function () {
-  var ordertype = Session.get('ordertype')
-  var currency = Session.get('currency')
-  var token = Tokens.findOne(currency)
-  var balance = new BigNumber(token.balance)
-  var allowance = new BigNumber(token.allowance)
-  return ordertype === 'buy' ? web3.fromWei(BigNumber.min(balance, allowance).toString(10)) : Infinity
-}
-
-var amountMax = function () {
-  var ordertype = Session.get('ordertype')
-  var token = Tokens.findOne('MKR')
-  var balance = new BigNumber(token.balance)
-  var allowance = new BigNumber(token.allowance)
-  return ordertype === 'sell' ? web3.fromWei(BigNumber.min(balance, allowance).toString(10)) : Infinity
-}
-
-Template.neworder.helpers({
-  ordertype: function () {
-    return Session.get('ordertype')
+Template.neworder.viewmodel({
+  type: 'buy',
+  fancyType: function () {
+    return this.type() === 'buy' ? 'Bid' : 'Ask'
   },
-  currency: function () {
-    return Session.get('currency')
-  },
-  price: function () {
-    return Session.get('price')
-  },
-  amount: function () {
-    return Session.get('amount')
-  },
-  total: function () {
-    return Session.get('total')
-  },
-  totalMax: totalMax,
-  amountMax: amountMax,
-  buttonState: function () {
-    var ordertype = Session.get('ordertype')
-    var price = new BigNumber(Session.get('price'))
-    var amount = new BigNumber(Session.get('amount'))
-    var total = new BigNumber(Session.get('total'))
-    if (price.gt(0) && amount.gt(0) && total.gt(0) && (ordertype !== 'buy' || total.lte(new BigNumber(totalMax()))) && (ordertype !== 'sell' || amount.lte(new BigNumber(amountMax())))) {
-      return ''
-    } else {
-      return 'disabled'
-    }
-  }
-})
-
-Template.neworder.events({
-  'change .ordertype': function () {
-    var ordertype = $('input:radio[name="ordertype"]:checked').val()
-    Session.set('ordertype', ordertype)
-  },
-  'change .currency': function () {
-    var currency = $('input:radio[name="currency"]:checked').val()
-    Session.set('currency', currency)
-  },
-  'change .price, keyup .price, mouseup .price': function () {
-    var price = $('input[name="price"]').val()
-    var amount = Session.get('amount')
-    Session.set('price', price)
-    Session.set('total', amount * price)
-  },
-  'change .amount, keyup .amount, mouseup .amount': function () {
-    var amount = $('input[name="amount"]').val()
-    var price = Session.get('price')
-    Session.set('amount', amount)
-    Session.set('total', amount * price)
-  },
-  'change .total, keyup .total, mouseup .total': function () {
-    var total = $('input[name="total"]').val()
-    var price = Session.get('price')
-    Session.set('total', total)
-    if (price > 0) {
-      Session.set('amount', total / price)
-    } else {
-      Session.set('amount', 0)
+  currency: 'ETH',
+  price: '0',
+  amount: '0',
+  calcTotal: function (event) {
+    try {
+      var price = new BigNumber(this.price())
+      var amount = new BigNumber(this.amount())
+      var total = price.times(amount)
+      total.isNaN() ? this.total('0') : this.total(total.toString(10))
+    } catch (e) {
+      this.total('0')
     }
   },
-  'click #submitorder': function (event) {
+  total: '0',
+  calcAmount: function (event) {
+    try {
+      var price = new BigNumber(this.price())
+      var amount = new BigNumber(this.amount())
+      var total = new BigNumber(this.total())
+      if (total.isZero() && price.isZero() && (amount.isNaN() || amount.isNegative())) {
+        this.amount('0')
+      } else if (!total.isZero() || !price.isZero()) {
+        amount = total.div(price)
+        console.debug(price.toString(), total.toString(), amount.toString())
+        amount.isNaN() ? this.amount('0') : this.amount(amount.toString(10))
+      }
+    } catch (e) {
+      this.amount('0')
+    }
+  },
+  maxAmount: function () {
+    var token = Tokens.findOne('MKR')
+    var balance = new BigNumber(token.balance)
+    var allowance = new BigNumber(token.allowance)
+    return this.type() === 'sell' ? web3.fromWei(BigNumber.min(balance, allowance).toString(10)) : Infinity
+  },
+  maxTotal: function () {
+    // Only allow change of total if price is well-defined
+    try {
+      var price = new BigNumber(this.price())
+      if ((price.isNaN() || price.isZero() || price.isNegative())) {
+        return '0'
+      }
+    } catch (e) {
+      return '0'
+    }
+    // If price is well-defined, take minimum of balance and allowance of currency, if 'buy', otherwise Infinity
+    var token = Tokens.findOne(this.currency())
+    var balance = new BigNumber(token.balance)
+    var allowance = new BigNumber(token.allowance)
+    return this.type() === 'buy' ? web3.fromWei(BigNumber.min(balance, allowance).toString(10)) : Infinity
+  },
+  canSubmit: function () {
+    try {
+      var type = this.type()
+      var price = new BigNumber(this.price())
+      var amount = new BigNumber(this.amount())
+      var maxAmount = new BigNumber(this.maxAmount())
+      var total = new BigNumber(this.total())
+      var maxTotal = new BigNumber(this.maxTotal())
+      return price.gt(0) && amount.gt(0) && total.gt(0) && (type !== 'buy' || total.lte(maxTotal)) && (type !== 'sell' || amount.lte(maxAmount))
+    } catch (e) {
+      return false
+    }
+  },
+  submit: function (event) {
     event.preventDefault()
 
     var sell_how_much, sell_which_token, buy_how_much, buy_which_token
-    if (Session.get('ordertype') === 'buy') {
-      sell_how_much = web3.toWei(Session.get('total'))
-      sell_which_token = Session.get('currency')
-      buy_how_much = web3.toWei(Session.get('amount'))
+    if (this.type() === 'buy') {
+      sell_how_much = web3.toWei(this.total())
+      sell_which_token = this.currency()
+      buy_how_much = web3.toWei(this.amount())
       buy_which_token = BASE_CURRENCY
     } else {
-      sell_how_much = web3.toWei(Session.get('amount'))
+      sell_how_much = web3.toWei(this.amount())
       sell_which_token = BASE_CURRENCY
-      buy_how_much = web3.toWei(Session.get('total'))
-      buy_which_token = Session.get('currency')
+      buy_how_much = web3.toWei(this.total())
+      buy_which_token = this.currency()
     }
     Offers.newOffer(sell_how_much, sell_which_token, buy_how_much, buy_which_token)
   }
