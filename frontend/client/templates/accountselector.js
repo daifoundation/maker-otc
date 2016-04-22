963049f36009d0a58e168edd3dfdb64408b77a22
Joris Bontje <jorisbontje@gmail.com>
2016-04-22 23:25:29 +0200

    account selection. closes #14

---
+++ frontend/client/templates/accountselector.js
@@ -0,0 +1,13 @@
+Template.accountSelector.helpers({
+  accounts: function () {
+    return web3.eth.accounts
+  }
+})
+
+Template.accountSelector.events({
+  'change': function (event, template) {
+    Session.set('address', event.target.value)
+    web3.eth.defaultAccount = event.target.value
+    Tokens.sync()
+  }
+})
