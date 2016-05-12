a19dfe66d13e867a81797c7a165dfb690861e13d
rainbeam <rainbeam@riseup.net>
2016-05-12 14:07:42 +0100

    btc market: add offer

--- contracts/btc_market.sol
+++ contracts/btc_market.sol
@@ -7,5 +7,48 @@ import 'simple_market.sol';
 // BTC-relay integration
 
 contract BTCMarket is MakerUser, EventfulMarket, FallbackFailer, Assertive {
+    struct OfferInfo {
+        uint sell_how_much;
+        bytes32 sell_which_token;
+        uint buy_how_much;
+        bytes32 buy_which_token;
+        address owner;
+        bool active;
+    }
+    uint public last_offer_id;
+    mapping( uint => OfferInfo ) public offers;
+
     function BTCMarket( MakerUserLinkType registry ) MakerUser( registry ) {}
+
+    function next_id() internal returns (uint) {
+        last_offer_id++; return last_offer_id;
+    }
+    function offer( uint sell_how_much, bytes32 sell_which_token
+                  , uint buy_how_much,  bytes32 buy_which_token )
+        returns (uint id)
+    {
+        assert(sell_how_much > 0);
+        assert(sell_which_token != 0x0);
+        assert(buy_how_much > 0);
+        assert(buy_which_token > 0);
+
+        transferFrom( msg.sender, this, sell_how_much, sell_which_token );
+        OfferInfo memory info;
+        info.sell_how_much = sell_how_much;
+        info.sell_which_token = sell_which_token;
+        info.buy_how_much = buy_how_much;
+        info.buy_which_token = buy_which_token;
+        info.owner = msg.sender;
+        info.active = true;
+        id = next_id();
+        offers[id] = info;
+        ItemUpdate(id);
+        return id;
+    }
+    function getOffer( uint id ) constant
+        returns (uint, bytes32, uint, bytes32) {
+      var offer = offers[id];
+      return (offer.sell_how_much, offer.sell_which_token,
+              offer.buy_how_much, offer.buy_which_token);
+    }
 }
--- contracts/btc_market_test.sol
+++ contracts/btc_market_test.sol
@@ -15,4 +15,18 @@ contract BTCMarketTest is Test
         user1.doApprove(otc, 100, "DAI");
         approve(otc, 30, "MKR");
     }
+    function testOfferBuyBitcoin() {
+        var id = otc.offer(30, "MKR", 10, "BTC");
+        assertEq(id, 1);
+        assertEq(otc.last_offer_id(), id);
+
+        var (sell_how_much, sell_which_token,
+             buy_how_much, buy_which_token) = otc.getOffer(id);
+
+        assertEq(sell_how_much, 30);
+        assertEq32(sell_which_token, "MKR");
+
+        assertEq(buy_how_much, 10);
+        assertEq32(buy_which_token, "BTC");
+    }
 }
