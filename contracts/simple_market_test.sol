550cd7426059ae36bce8a627b17d1fe14c0799ed
rain <rainbeam@riseup.net>
2016-07-19 22:07:21 +0100

    add expiring market transfer tests

--- contracts/simple_market_test.sol
+++ contracts/simple_market_test.sol
@@ -521,3 +521,47 @@ contract ExpiringMarketTest is Test {
         user1.doBuy(id, 30);
     }
 }
+
+contract ExpiringTransferTest is TransferTest {
+    function setUp() {
+        otc = new ExpiringMarket(1 weeks);
+        user1 = new MarketTester();
+        user1.bindMarket(otc);
+
+        dai = new ERC20Base(10 ** 9);
+        mkr = new ERC20Base(10 ** 6);
+
+        dai.transfer(user1, 100);
+        user1.doApprove(otc, 100, dai);
+        mkr.approve(otc, 30);
+    }
+}
+
+contract ExpiringOfferTransferTest is OfferTransferTest, ExpiringTransferTest {}
+contract ExpiringBuyTransferTest is BuyTransferTest, ExpiringTransferTest {}
+contract ExpiringPartialBuyTransferTest is PartialBuyTransferTest, ExpiringTransferTest {}
+
+contract ExpiringCancelTransferTest is CancelTransferTest
+                                     , ExpiringTransferTest
+{
+    function testCancelAfterExpiryTransfersFromMarket() {
+        var id = otc.offer( 30, mkr, 100, dai );
+        otc.addTime(1 weeks + 1 seconds);
+
+        var balance_before = mkr.balanceOf(otc);
+        otc.cancel(id);
+        var balance_after = mkr.balanceOf(otc);
+
+        assertEq(balance_before - balance_after, 30);
+    }
+    function testCancelAfterExpiryTransfersToSeller() {
+        var id = otc.offer( 30, mkr, 100, dai );
+        otc.addTime(1 weeks + 1 seconds);
+
+        var balance_before = mkr.balanceOf(this);
+        otc.cancel(id);
+        var balance_after = mkr.balanceOf(this);
+
+        assertEq(balance_after - balance_before, 30);
+    }
+}
