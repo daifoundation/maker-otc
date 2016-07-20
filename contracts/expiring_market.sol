7e0801dcef2d8be502a5dcbafd14e9cfe86d497f
rain <rainbeam@riseup.net>
2016-07-20 10:47:29 +0100

    comments

--- contracts/expiring_market.sol
+++ contracts/expiring_market.sol
@@ -1,6 +1,6 @@
 import 'simple_market.sol';
 
-// Simple Market with a market lifetime. When the lifetime has expired,
+// Simple Market with a market lifetime. When the lifetime has elapsed,
 // offers can only be cancelled (offer and buy will throw).
 
 contract ExpiringMarket is SimpleMarket {
@@ -15,17 +15,18 @@ contract ExpiringMarket is SimpleMarket {
         return (getTime() > close_time);
     }
 
-    // after market lifetime has expired, no new offers are allowed
+    // after market lifetime has elapsed, no new offers are allowed
     modifier can_offer {
         assert(!isClosed());
         _
     }
+    // after close, no new buys are allowed
     modifier can_buy(uint id) {
         assert(isActive(id));
         assert(!isClosed());
         _
     }
-    // after expiry, anyone can cancel an offer
+    // after close, anyone can cancel an offer
     modifier can_cancel(uint id) {
         assert(isActive(id));
         assert(isClosed() || (msg.sender == getOwner(id)));
