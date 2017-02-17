212fb90f542710457c53b4bcc4be8b372ecc18a8
Daniel Brockman <daniel@brockman.se>
2017-02-17 18:02:50 -0500

    memes

--- src/simple_market.sol
+++ src/simple_market.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.4.8;
 
 import "erc20/erc20.sol";
 
-// A simple direct exchange order manager.
-
 contract EventfulMarket {
     event ItemUpdate( uint id );
     event Trade( uint sell_how_much, address indexed sell_which_token,
@@ -28,7 +26,7 @@ contract EventfulMarket {
         uint128           giveAmount
     );
 
-    event LogCancel(
+    event LogKill(
         bytes32           id,
         address  indexed  maker,
         address  indexed  haveToken,
@@ -37,28 +35,29 @@ contract EventfulMarket {
 }
 
 contract SimpleMarket is EventfulMarket {
-    bool private lock;
+    bool locked;
 
-    modifier exclusive {
-        if (lock) throw;
-        lock = true;
+    modifier synchronized {
+        assert(!locked);
+        locked = true;
         _;
-        lock = false;
+        locked = false;
     }
 
-    function assert(bool assertion) internal {
-        if (!assertion) throw;
+    function assert(bool x) internal {
+        if (!x) throw;
     }
 
     struct OfferInfo {
-        uint sell_how_much;
-        ERC20 sell_which_token;
-        uint buy_how_much;
-        ERC20 buy_which_token;
-        address owner;
-        bool active;
+        uint     sell_how_much;
+        ERC20    sell_which_token;
+        uint     buy_how_much;
+        ERC20    buy_which_token;
+        address  owner;
+        bool     active;
     }
-    mapping( uint => OfferInfo ) public offers;
+
+    mapping (uint => OfferInfo) public offers;
 
     uint public last_offer_id;
 
@@ -126,8 +125,8 @@ contract SimpleMarket is EventfulMarket {
     function take(bytes32 id, uint128 maxTakeAmount) {
         assert(buy(uint256(id), maxTakeAmount));
     }
-    
-    function cancel(bytes32 id) {
+
+    function kill(bytes32 id) {
         assert(cancel(uint256(id)));
     }
 
@@ -135,7 +134,7 @@ contract SimpleMarket is EventfulMarket {
     function offer( uint sell_how_much, ERC20 sell_which_token
                   , uint buy_how_much,  ERC20 buy_which_token )
         can_offer
-        exclusive
+        synchronized
         returns (uint id)
     {
         assert(uint128(sell_how_much) == sell_how_much);
@@ -174,7 +173,7 @@ contract SimpleMarket is EventfulMarket {
     // offer maker, and from market to caller.
     function buy( uint id, uint quantity )
         can_buy(id)
-        exclusive
+        synchronized
         returns ( bool success )
     {
         assert(uint128(quantity) == quantity);
@@ -237,7 +236,7 @@ contract SimpleMarket is EventfulMarket {
     // Cancel an offer. Refunds offer maker.
     function cancel( uint id )
         can_cancel(id)
-        exclusive
+        synchronized
         returns ( bool success )
     {
         // read-only offer. Modify an offer by directly accessing offers[id]
@@ -248,7 +247,7 @@ contract SimpleMarket is EventfulMarket {
         assert(seller_refunded);
 
         ItemUpdate(id);
-        LogCancel(
+        LogKill(
             bytes32(id),
             offer.owner,
             uint128(offer.sell_which_token),
