55cd4a6b4970ab52f59b1402a40560adcaed3af8
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-07-30 15:58:52 -0300

    No more option to disable cherry picking offers

--- src/matching_market.t.sol
+++ src/matching_market.t.sol
@@ -24,19 +24,6 @@ contract MarketTester {
     {
         return market.getNextUnsortedOffer(mid);
     }
-    function doSetBuyEnabled(bool ebu_)
-        public
-        returns (bool)
-    {
-        return market.setBuyEnabled(ebu_);
-    }
-    function doIsBuyEnabled()
-        public
-        view
-        returns (bool)
-    {
-        return market.buyEnabled();
-    }
     function doSetMinSellAmount(ERC20 pay_gem, uint min_amount)
         public
         returns (bool)
@@ -497,46 +484,6 @@ contract OrderMatchingTest is DSTest, EventfulMarket, MatchingEvents {
         mkr.approve(otc, 30);
         offer_id[1] = otc.offer(30, mkr, 100, dai, 0);
         otc.insert(offer_id[1],7);  //there is no active offer at pos 7
-    }
-    function testBuyEnabledByDefault() public view {
-        assert(otc.buyEnabled());
-    }
-    function testSetBuyDisabled() public {
-        otc.setBuyEnabled(false);
-        assert(!otc.buyEnabled());
-        expectEventsExact(otc);
-        emit LogBuyEnabled(false);
-    }
-    function testSetBuyEnabled() public {
-        otc.setBuyEnabled(false);
-        otc.setBuyEnabled(true);
-        assert(otc.buyEnabled());
-        expectEventsExact(otc);
-        emit LogBuyEnabled(false);
-        emit LogBuyEnabled(true);
-    }
-    function testFailBuyDisabled() public {
-        otc.setBuyEnabled(false);
-        mkr.approve(otc, 30);
-        dai.transfer(user1, 100 );
-        offer_id[1] = otc.offer(30, mkr, 100, dai, 0);
-        user1.doBuy(offer_id[1], 30);//should fail
-    }
-    function testBuyEnabledBuyWorks() public {
-        otc.setBuyEnabled(false);
-        otc.setBuyEnabled(true);
-        mkr.approve(otc, 30);
-        dai.transfer(user1, 90);
-        user1.doApprove(otc, 90, dai);
-        offer_id[1] = otc.offer(30, mkr, 90, dai, 0);
-        user1.doBuy(offer_id[1], 30);
-
-        expectEventsExact(otc);
-        emit LogBuyEnabled(false);
-        emit LogBuyEnabled(true);
-        emit LogItemUpdate(offer_id[1]);
-        emit LogTrade(30, mkr, 100, dai);
-        emit LogItemUpdate(offer_id[1]);
     }
     function testSetGetMinSellAmout() public {
         otc.setMinSell(dai, 100);
