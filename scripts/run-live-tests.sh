92b4263662b70355c30212b7cede768316d0863f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-01-15 15:43:41 -0300

    Don't run mainnet forked test on run-tests.sh

--- scripts/run-live-tests.sh
+++ scripts/run-live-tests.sh
@@ -6,11 +6,11 @@ cd ..
 dapp --use solc:0.5.12 build
 
 
-echo "Testing on forked mainnet"
+echo "Testing on forked mainnet (with txt.origin == msg.sender)"
 export DAPP_TEST_ORIGIN=0x3be95e4159a131e56a84657c4ad4d43ec7cd865d # fool otc contract that we do direct calls
 export DAPP_TEST_NUMBER=11607594
-hevm dapp-test --json-file=out/dapp.sol.json --rpc=https://parity-mainnet.makerfoundation.com:8545 --match live --verbose 1
+hevm dapp-test --json-file=out/dapp.sol.json --rpc=https://parity-mainnet.makerfoundation.com:8545 --match "origin"
 
-echo "Testing indirect calls on forked mainnet"
+echo "Testing indirect calls on forked mainnet (txt.origin != msg.sender)"
 export DAPP_TEST_ORIGIN=0x0
-hevm dapp-test --json-file=out/dapp.sol.json --rpc=https://parity-mainnet.makerfoundation.com:8545 --match Indirect
+hevm dapp-test --json-file=out/dapp.sol.json --rpc=https://parity-mainnet.makerfoundation.com:8545 --match "indirect"
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,14 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
+LIVE_TESTS_FILE="matching_market_live.t.sol"
+
+function clean() {
+    mv "/tmp/$LIVE_TESTS_FILE" ./src/
+}
+trap clean EXIT
+
+mv "./src/$LIVE_TESTS_FILE" /tmp
+
 export DAPP_TEST_ORIGIN=0x3be95e4159a131e56a84657c4ad4d43ec7cd865d # this address is needed to fool smart contract to think that we are making a direct calls (HEVM limitation)
-# TODO: It should exclude the live tests
 dapp --use solc:0.5.12 test
