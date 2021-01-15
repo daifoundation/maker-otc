4c337fb1005089bdf1aa289b51177e36c498026f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-01-15 12:36:48 -0300

    Compile with solc 0.5.12

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,5 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-dapp build
-
 export DAPP_TEST_ORIGIN=0xb353c6c70829fd39756d165f363f72008b9ae1e3 # this address is needed to fool smart contract to think that we are making a direct calls (HEVM limitation)
-dapp test
+dapp --use solc:0.5.12 test
