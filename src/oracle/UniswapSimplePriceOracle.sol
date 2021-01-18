6bbb4754b40a4f579034d7a24a0d8725c4f2a965
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-01-18 10:54:54 -0300

    Simplify uniswap files

--- src/oracle/UniswapSimplePriceOracle.sol
+++ src/oracle/UniswapSimplePriceOracle.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0;
+pragma solidity ^0.5.12;
 
 import "./UniswapLibrary.sol";
 
@@ -9,10 +9,8 @@ contract UniswapSimplePriceOracle {
         uniswapFactory = _uniswapFactory;
     }
 
-    function getPriceFor(address tokenA, address tokenB, uint256 tokenAAmt) public view returns (uint256) {
-      (uint reserve0, uint reserve1) = UniswapV2Library.getReserves(uniswapFactory, tokenA, tokenB);
-        uint256 dust = UniswapV2Library.getAmountOut(tokenAAmt, reserve0, reserve1);
-
-        return dust;
+    function getPriceFor(address tokenA, address tokenB, uint256 tokenAAmt) public view returns (uint256 dust) {
+        (uint reserve0, uint reserve1) = UniswapV2Library.getReserves(uniswapFactory, tokenA, tokenB);
+        dust = UniswapV2Library.getAmountOut(tokenAAmt, reserve0, reserve1);
     }
 }
